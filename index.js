9c8eafea30a48aa3609df099347733839897bf2a
devberiluysal <nazberiluysal@gmail.com>
2023-08-07 21:22:05 +0300

    arrow function

--- index.js
+++ index.js
@@ -1,51 +1,54 @@
 function computerPlay() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
-  }
+    const choices = ['Rock', 'Paper', 'Scissors']
+    const randomIndex = Math.floor(Math.random() * choices.length)
+    return choices[randomIndex]
+}
   
 function playRound(playerSelection, computerSelection) {
-    const playerChoice = playerSelection.toLowerCase();
+    const winner = {
+        rock: 'scissors',
+        paper: 'rock',
+        scissors: 'paper'
+    }
+    const playerChoice = playerSelection.toLowerCase()
+    const computerChoice = computerSelection.toLowerCase()
 
-    if (playerChoice === computerSelection.toLowerCase()) {
-        return "It's a tie!";
-    } else if (
-        (playerChoice === 'rock' && computerSelection === 'Scissors') ||
-        (playerChoice === 'paper' && computerSelection === 'Rock') ||
-        (playerChoice === 'scissors' && computerSelection === 'Paper')
-    ) {
-        return `Congrats, you win! ${playerSelection} beats ${computerSelection}`;
+    if (playerChoice === computerChoice) {
+        return "It's a tie!"
+    } else if (winner[playerChoice] === computerChoice) {
+        return `Congrats, you win! ${playerSelection} beats ${computerSelection}`
     } else {
-        return `Sorry :( You lose! ${computerSelection} beats ${playerSelection}`;
+        return `Sorry :( You lose! ${computerSelection} beats ${playerSelection}`
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let round = 1;
+const gameFunction = () => {
+    let playerScore = 0
+    let computerScore = 0
+    let round = 1
     console.log('Get 5 score and finish the game.')
-    while(playerScore !== 5){
-      const playerSelection = prompt(`Round ${round}: Enter your choice: Rock, Paper, or Scissors`);
-      const computerSelection = computerPlay();
-      const result = playRound(playerSelection, computerSelection);
+    
+    while(round <= 5){
+      const playerSelection = prompt(`Round ${round}: Enter your choice: Rock, Paper, or Scissors`)
+      const computerSelection = computerPlay()
+      const result = playRound(playerSelection, computerSelection)
   
-      console.log(result);
+      console.log(result)
   
       if (result.includes('win')) {
-        playerScore++;
+        playerScore++
         console.log('Your score: ' + playerScore)
-        round++;
+        round++
       } else if (result.includes('lose')) {
-        computerScore++;
+        computerScore++
         console.log('Computer score: ' + computerScore)
+        round++
       }
       if(playerScore == 5) {
         console.log('Finally your score is 5!')
       }
-
     }
-  }
-  document.addEventListener('DOMContentLoaded', () => game())
+}
+document.addEventListener('DOMContentLoaded', () => gameFunction())
 
   
