54a95f3def06a193cee3e0062d745c2ae2338056
devberiluysal <nazberiluysal@gmail.com>
2023-08-08 18:52:29 +0300

    iife function

--- index.js
+++ index.js
@@ -1,54 +1,77 @@
-function computerPlay() {
-    const choices = ['Rock', 'Paper', 'Scissors']
-    const randomIndex = Math.floor(Math.random() * choices.length)
-    return choices[randomIndex]
-}
-  
-function playRound(playerSelection, computerSelection) {
+const gameResult = (function () {
+  function computerPlay() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+  }
+
+  function playRound(playerSelection, computerSelection) {
     const winner = {
-        rock: 'scissors',
-        paper: 'rock',
-        scissors: 'paper'
-    }
-    const playerChoice = playerSelection.toLowerCase()
-    const computerChoice = computerSelection.toLowerCase()
+      rock: "scissors",
+      paper: "rock",
+      scissors: "paper",
+    };
+
+    const playerChoice = playerSelection.toLowerCase();
+    const computerChoice = computerSelection.toLowerCase();
 
     if (playerChoice === computerChoice) {
-        return "It's a tie!"
+      return "It's a tie!";
     } else if (winner[playerChoice] === computerChoice) {
-        return `Congrats, you win! ${playerSelection} beats ${computerSelection}`
+      return `Congrats, you win! ${playerSelection} beats ${computerSelection}`;
     } else {
-        return `Sorry :( You lose! ${computerSelection} beats ${playerSelection}`
+      return `Sorry :( You lose! ${computerSelection} beats ${playerSelection}`;
     }
-}
+  }
 
-const gameFunction = () => {
-    let playerScore = 0
-    let computerScore = 0
-    let round = 1
-    console.log('Get 5 score and finish the game.')
-    
-    while(round <= 5){
-      const playerSelection = prompt(`Round ${round}: Enter your choice: Rock, Paper, or Scissors`)
-      const computerSelection = computerPlay()
-      const result = playRound(playerSelection, computerSelection)
-  
-      console.log(result)
-  
-      if (result.includes('win')) {
-        playerScore++
-        console.log('Your score: ' + playerScore)
-        round++
-      } else if (result.includes('lose')) {
-        computerScore++
-        console.log('Computer score: ' + computerScore)
-        round++
-      }
-      if(playerScore == 5) {
-        console.log('Finally your score is 5!')
-      }
+  function finalResult(playerScore, computerScore) {
+    if (playerScore > computerScore) {
+      return "Congrats!! Do you have a magic ball?";
+    } else {
+      return "AAhhhh!! Never mind, come and try again :/";
     }
-}
-document.addEventListener('DOMContentLoaded', () => gameFunction())
+  }
 
-  
+  function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let round = 1;
+    console.log("Get 5 score and finish the game.");
+
+    while (round < 6) {
+      const playerSelection = prompt(
+        `Round ${round}: Enter your choice: Rock, Paper, or Scissors`
+      );
+      if (playerSelection === null) {
+        console.log(
+          "Game has been canceled by the user. To start the game please call the function from console or refresh the page."
+        );
+        break;
+      }
+      const computerSelection = computerPlay();
+      const result = playRound(playerSelection, computerSelection);
+
+      console.log(result);
+
+      if (result.includes("win")) {
+        playerScore++;
+        round++;
+      } else if (result.includes("lose")) {
+        computerScore++;
+        round++;
+      }
+      console.log(
+        "Computer score:",
+        computerScore,
+        "\nYour score:",
+        playerScore
+      );
+    }
+    if (!(round < 6)) {
+      console.log(finalResult(playerScore, computerScore));
+    }
+  }
+  return game;
+})();
+
+gameResult();
